6e5da9e2d7a3fe332a4ea8253a5bed479267ec1a
Imam Zakky <imam.zakky.ft18@mail.umy.ac.id>
2020-12-25 20:50:27 +0700

    Add project files.

---
+++ Server_047_ImamZakky/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service_047_ImamZakky;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace Server_047_ImamZakky
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
